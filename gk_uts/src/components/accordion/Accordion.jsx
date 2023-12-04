4f23f2e7e13130a4d971f7aaee47881bf1e1bccb
Ekateryna Sholomickaya <shan_lee@tut.by>
2023-12-04 14:37:59 +0300

    add block accordion

---
+++ gk_uts/src/components/accordion/Accordion.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import styles from './Accordion.module.css';
+import { AccordionItem } from './AccordionItem';
+
+export const Accordion = ({ faqList }) => {
+  const [openId, setId] = useState(null);
+
+
+  return (
+    <ul className={styles.accordion}>
+      {faqList.map((faqItem, id) => {
+        return (
+          <AccordionItem
+            onClick={() => (id === openId ? setId(null) : setId(id))}
+            faqItem={faqItem}
+            isOpen={id === openId}
+            key={id} />
+        )
+      })}
+    </ul>
+  )
+}
---
+++ gk_uts/src/components/accordion/AccordionItem.jsx
@@ -0,0 +1,31 @@
+import { useRef } from 'react';
+import React from "react";
+import styles from './Accordion.module.css';
+import { ReactComponent as ArrowIcon } from '../../assets/icons/arrow.svg';
+
+
+
+export const AccordionItem = ({ faqItem, onClick, isOpen }) => {
+
+  const itemRef = useRef(null);
+
+  return (
+    <li className={styles.accordin__item} >
+      <button
+        className={styles.accordion__header}
+        onClick={() => onClick()}>
+        {faqItem.q}
+        <ArrowIcon className={`${styles.according__arrow} ${isOpen ? styles.active : ""}`} />
+      </button>
+      <div
+        className={`${styles.accordin__collapse} `}
+        style={
+          isOpen ? { height: itemRef.current.scrollHeight } : { height: "0px" }} >
+        <div className={styles.accordion__body} ref={itemRef}>
+          {faqItem.a}
+        </div>
+      </div>
+
+    </li>
+  )
+}
