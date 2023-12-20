a6006e996d862d052a52642fe2010833f04f7dba
Ekateryna Sholomickaya <shan_lee@tut.by>
2023-12-20 16:02:08 +0300

    add smooth for button to top

--- gk_uts/src/components/main/Main.jsx
+++ gk_uts/src/components/main/Main.jsx
@@ -2,15 +2,39 @@ import styles from './Main.module.css';
 import arrowImg from '../../assets/icons/main/arrow.svg';
 
 import swipeup from '../../assets/icons/swipeup.svg';
+import { useEffect, useState } from 'react';
 
 
-const Main = () =>
-  <section className={styles.main}>
+const Main = () => {
+
+  const [backToTop, setBackToTop] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        setBackToTop(true);
+      }
+      else {
+        setBackToTop(false);
+      }
+    })
+  })
+
+  const scrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+
+  return (<section className={styles.main}>
     <div className="container">
       <div className={styles.main__arrow}>
-        <a href="#logo" className={styles.arrow}>
-          <img src={swipeup} alt="arrow" className={styles.arrow_img} />
-        </a>
+        {backToTop && <button onClick={scrollUp}>
+          {/* <a href="#" className={styles.arrow}> */}
+          <img src={swipeup} alt="arrow" className={styles.arrow} />
+          {/* </a> */}
+        </button>}
       </div>
 
       <div className={styles.main__column}>
@@ -32,6 +56,6 @@ const Main = () =>
     </div>
 
 
-  </section>
-
+  </section>)
+}
 export default Main;
