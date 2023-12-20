79a432c1b80304994088048390f49b376a97f6f9
Ekateryna Sholomickaya <shan_lee@tut.by>
2023-12-20 16:35:44 +0300

    footer

--- gk_uts/src/components/footer/Footer.jsx
+++ gk_uts/src/components/footer/Footer.jsx
@@ -1,5 +1,8 @@
 import styles from './Footer.module.css';
 import logo from '../../assets/icons/logoWhite.svg';
+import { ReactComponent as Linked } from '../../assets/icons/linked.svg';
+import { ReactComponent as Youtube } from '../../assets/icons/youtube.svg';
+
 import linked from '../../assets/icons/linked.svg';
 import youtube from '../../assets/icons/youtube.png';
 
@@ -17,8 +20,15 @@ const Footer = () =>
           <p className={styles.footer__text} >Крупнейший поставщик химического сырья и пищевых ингредиентов в России и странах СНГ </p>
         </div>
         <div className={styles.footer__links}>
-          <img src={linked} alt="linked" />
-          <img src={youtube} alt="youtube" />
+          <a href="https://www.linkedin.com/company/uts-group">
+            {/* <Linked alt="linked" className={styles.footer__links_img} /> */}
+            <img src={linked} alt="linked" className={styles.footer__links_img} />
+          </a>
+          <a href="https://www.youtube.com/channel/UCS9oyiwPEvdPWLTG9us8DHQ">
+            {/* <Youtube className={styles.footer__links_img} />
+            */}
+            <img src={youtube} className={styles.footer__links_img} />
+          </a>
         </div>
         <div className={styles.footer__address}>
           <div className={styles.footer__title}>Адрес:</div>
@@ -27,7 +37,7 @@ const Footer = () =>
         <div className={styles.footer__phones}>
           <div className={styles.footer__title}>Телефоны:</div>
           <a href="">+7 (812) 389 55 55</a><br />
-          <a href="">+7 (812) 389 55 55</a>
+          <a href="">+7 (812) 426 74 47</a>
         </div>
 
 
